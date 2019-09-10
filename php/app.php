ac29c1d2d7410118c160741726158c2d442f20f1
knnisha <54218694+knnisha@users.noreply.github.com>
2019-09-10 22:31:01 +0530

    Add files via upload

---
+++ php/app.php
@@ -0,0 +1,14 @@
+
+<html>
+<body>
+<b>Your application</b><br />
+Name: <?php echo $_POST['name']; ?><br />
+Date of Birth: <?php echo $_POST['dob']; ?><br />
+Contact no: <?php echo $_POST['cno']; ?><br />
+<br />
+Gender: <?php echo $_POST['gender']; ?><br />
+<br />
+Remark:<br />
+<?php echo $_POST['remark']; ?>
+</body>
+</html>
